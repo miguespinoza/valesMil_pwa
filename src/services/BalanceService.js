0b5b4d470f1803035dc3d73ce222c1a14ce1d370
Miguel Espinoza <miguespinoza1@gmail.com>
2019-06-09 01:03:37 -0500

    change endpoint

--- src/services/BalanceService.js
+++ src/services/BalanceService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const fakeAPI = 'http://private-094e16-valesmil.apiary-mock.com/api/valesmil?code=1EcCTvok6a9XXKd6CjzoV2dziSIJgONKHQo5RTg105hnFPDTVTgT6g==';
-const realAPI = 'https://valesmil2.azurewebsites.net/api/valesmil?code=1EcCTvok6a9XXKd6CjzoV2dziSIJgONKHQo5RTg105hnFPDTVTgT6g==';
+const realAPI = 'https://valesmilapi2.azurewebsites.net/balance';
 
 class BalanceService{
     getBalance = ({
